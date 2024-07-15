552a4a1305d322422d65f53964f33436efbf849b
sonson <son@hotmai.com>
2024-07-15 08:08:34 +0000

    [update] done

--- deployment/db-service/02_seeding_data.sh
+++ deployment/db-service/02_seeding_data.sh
@@ -1,7 +1,7 @@
 export DB_PASSWORD=password
-export DB_HOST=127.0.0.1
+export DB_HOST=10.100.13.39
 export DB_USER=user
 export DB_NAME=ha-app
-PGPASSWORD="$DB_PASSWORD" psql --host $DB_HOST -U $DB_USER -d $DB_NAME -p 5433 < 1_create_table.sql
-PGPASSWORD="$DB_PASSWORD" psql --host $DB_HOST -U $DB_USER -d $DB_NAME -p 5433 < 2_seed_users.sql
-PGPASSWORD="$DB_PASSWORD" psql --host $DB_HOST -U $DB_USER -d $DB_NAME -p 5433 < 3_seed_tokens.sql
+PGPASSWORD="$DB_PASSWORD" psql --host $DB_HOST -U $DB_USER -d $DB_NAME -p 5432 < ../../db/1_create_tables.sql
+PGPASSWORD="$DB_PASSWORD" psql --host $DB_HOST -U $DB_USER -d $DB_NAME -p 5432 < ../../db/2_seed_users.sql
+PGPASSWORD="$DB_PASSWORD" psql --host $DB_HOST -U $DB_USER -d $DB_NAME -p 5432 < ../../db/3_seed_tokens.sql
